02177e05afa3e7986a7150689f277ca3668fa11b
Eduardo Santos Silva <eduardos_silva@hotmail.com>
2020-01-23 00:51:37 -0300

    Aprendendo a criar data frame

---
+++ Analise de dados/CriandodataFramecomSpider(parte2).py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+data = {
+        'alunos':['Eduardo', 'Lucas', 'Guilherme', 'Leozinho', 'Gabriel', 'Enrique'],
+        'idade':[24, 26, 20, 22, 20, 26],
+        'notas':[10, 10, 8, 7, 10, 6]
+        }
+
+
+df = pd.DataFrame(data, columns=['alunos', 'idade','notas'])
+
+
+df.describe()
+
+
+df.drop(5)
+
+df = df.drop(5)
+
+
+
+df.drop('idade', axis=1)
+
+
+df.count()
+
+df.columns
+
+df.shape
