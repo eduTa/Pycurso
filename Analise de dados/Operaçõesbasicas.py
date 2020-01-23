d955e431eb81e0a8ce449a7e0aba29885f640c68
Eduardo Santos Silva <eduardos_silva@hotmail.com>
2020-01-23 01:11:00 -0300

    Operações básicas

---
+++ Analise de dados/Operaçõesbasicas.py
@@ -0,0 +1,52 @@
+import pandas as pd
+
+data = {
+        'alunos':['Eduardo', 'Lucas', 'Guilherme', 'Leozinho', 'Gabriel', 'Enrique'],
+        'idade':[24, 26, 20, 22, 20, 26],
+        'notas':[10, 10, 8, 7, 10, 6]
+        }
+
+
+df = pd.DataFrame(data, columns=['alunos', 'idade','notas'])
+
+
+df.describe()
+
+
+df.drop(5)
+
+df = df.drop(5)
+
+
+
+df.drop('idade', axis=1)
+
+
+df.count()
+
+df.columns
+
+df.shape
+
+
+df.max()
+
+df['idade'].max()
+
+df.min()
+
+df.mean()
+
+df.median()
+
+df.sum()
+
+
+df['notas'].add(10)
+
+
+
+df['notas'].sub(10)
+
+
+df.mul(10)
